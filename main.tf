b1628cad5dfdf453c65e697a31422bfd14edf2d6
Munichandra <login4munichandra1@gmail.com>
2021-11-16 12:46:32 +0530

    enabled the host name

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ provider "aws" {
 # We need to create a vpc resource
 resource "aws_vpc" "awstf" {
     cidr_block = "192.168.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
     
      tags = {
     Name = "fromtf"
