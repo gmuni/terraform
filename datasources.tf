3ab53f64f30441d007aea51da2dfbf3548b959f5
Munichandra <login4munichandra1@gmail.com>
2021-11-19 11:22:47 +0530

    added dta source and for each for private subnet associations

--- datasources.tf
+++ datasources.tf
@@ -8,3 +8,14 @@ data "aws_subnet_ids" "publicsubnets" {
     }
 
 }
+
+# give me all the subnet with the tags app1, app2, db1 and db2
+# data source for subnet with tags app1, app2, db1 and db2
+data "aws_subnet_ids" "privatesubnets" {
+    vpc_id = aws_vpc.awstf.id
+
+    filter {
+      name = "tag:Name"
+      values = [local.subnets[2],local.subnets[3], local.subnets[4], local.subnets[5]]
+    }
+}
