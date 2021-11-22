e7374fbc53344d747443e4ce6a7dfc10bf092258
gmuni <33177375+gmuni@users.noreply.github.com>
2021-11-22 14:11:37 +0530

    Update backends.tf
    Added changes suit o worksapce

--- awsmodules/backends.tf
+++ awsmodules/backends.tf
@@ -1,9 +1,13 @@
 terraform {
   backend "s3" {
-      bucket = "forterraform1"
-      key = "terraform.tfstate"
+      bucket = "forterraform2"
+      key = "environments/terraform.tfstate"
       region = "us-west-2"
       dynamodb_table = "qtterraformlocks"
+
+      workspaces {
+          prefix = "lt-eapp-"
+      }
 
   }
 }
